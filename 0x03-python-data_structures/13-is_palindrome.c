0b53d19534a6e197f2f3a05a154023d2cd8562d2
Randy0607 <randall.julies89@gmail.com>
2022-08-28 19:55:36 +0100

    update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,15 +1,35 @@
+#include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
+ * is_palindrome - check if a linked list is a palindrome
  *
- * Return: Always 0.
+ * @head: first node
+ *
+ * Return: 1 if success
+ *         0 if failed
  */
-int main(void)
+int is_palindrome(listint_t **head)
 {
-	listint_t *head;
+	listint_t *tmp = *head;
+	int values[2048], i = 0, cLoop, limit;
 
-	return (0);
+	if (head == NULL || *head == NULL)
+		return (1);
+
+	while (tmp != NULL)
+	{
+		values[i] = tmp->n;
+		i++;
+		tmp = tmp->next;
+	}
+
+	limit = (i % 2 == 0) ? i / 2 : (i + 1) / 2;
+
+	for (cLoop = 0; cLoop < limit; cLoop++)
+		if (values[cLoop] != values[i - 1 - cLoop])
+			return (0);
+
+	return (1);
 }
