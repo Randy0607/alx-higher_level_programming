e3a43dd92d717f139f5a8d2e41ace2f2f80cbe46
Randy0607 <randall.julies89@gmail.com>
2022-09-26 09:06:20 +0100

    update

--- 0x0A-python-inheritance/10-main.py
+++ 0x0A-python-inheritance/10-main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 Square = __import__('10-square').Square
 
-s = Square(13)
-
-print(s)
-print(s.area())
+s = Square("13")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
